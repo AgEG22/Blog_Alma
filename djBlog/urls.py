37cde5914ee7385b41ddefcdcc6d272c4d2d224d
EgAG22 <eliasgomez331@gmial.com>
2022-08-20 20:50:14 -0300

    first commit

---
+++ djBlog/urls.py
@@ -0,0 +1,24 @@
+"""djBlog URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include   
+
+urlpatterns = [
+    path('admin/', admin.site.urls), # la urls del administrado de django.
+    path('', include('home.urls')), #en el espacio vacio de las comillas puede ir una direccion como 'home/' la cual este tendra que ir tambien con la direccion de la aplicacion web para que arrenque el servidor ejemplo: http://127.0.0.1:8000/home/
+                                    # la funcion include() lo que hace es buscarme la vista que quiero que renderice por medio de la ruta 'home.urls'.
+
+]
