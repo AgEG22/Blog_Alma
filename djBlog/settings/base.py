1bb26a6255b6396b37e7cf782071761798ed0788
EgAG22 <eliasgomez331@gmial.com>
2022-08-21 18:44:03 -0300

    se agrego los modelos categorias, post y comentario

--- djBlog/settings/base.py
+++ djBlog/settings/base.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'crispy_bootstrap5',    
     'accounts',
     'home',
+    'blog',
 ]
 
 MIDDLEWARE = [
@@ -127,6 +128,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'staticfiles'),
     os.path.join(os.path.dirname(BASE_DIR), "static"),
 ]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
